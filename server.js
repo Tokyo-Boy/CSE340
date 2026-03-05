050187ac6916843777afc2d9e4de91e926b895b8
Tokyo-Boy <haveyeverett@gmail.com>
2026-03-04 22:15:07 -0800

    Updated CSE340

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
@@ -15,6 +16,9 @@ const static = require("./routes/static")
  *************************/
 app.use(static)
 
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -27,4 +31,9 @@ const host = process.env.HOST
  *************************/
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
+})
+
+// index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
 })
