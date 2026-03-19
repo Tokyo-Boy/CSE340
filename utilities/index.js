2be07e3b6e10aa46498c0608036880072e26aa71
Tokyo-Boy <haveyeverett@gmail.com>
2026-03-18 17:05:18 -0700

    Fixed

--- utilities/index.js
+++ utilities/index.js
@@ -28,7 +28,7 @@ Util.getNav = async function (req, res, next) {
 * Build the classification view HTML
 * ************************************ */
 Util.buildClassificationGrid = async function(data){
-  let grid
+  let grid = "" 
   if(data.length > 0){
     grid = '<ul id="inv-display">'
     data.forEach(vehicle => { 
@@ -52,10 +52,9 @@ Util.buildClassificationGrid = async function(data){
     })
     grid += '</ul>'
   } else { 
-    grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+    grid = '<p class="notice">Sorry, no matching vehicles could be found.</p>'
   }
   return grid
 }
 
 module.exports = Util
-
