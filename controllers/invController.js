84b23c8c3825c41ae44a000e7fd88884894619bb
Tokyo-Boy <haveyeverett@gmail.com>
2026-03-19 05:50:15 -0700

    Fix: resolve view lookup error and messages undefined crash

--- controllers/invController.js
+++ controllers/invController.js
@@ -4,20 +4,27 @@ const utilities = require("../utilities/")
 const invCont = {}
 
 /* ***************************
- *  Build inventory by classification view
+ * Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id)
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
-  const className = data[0].classification_name
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-  })
-}
+  try {
+    const classification_id = req.params.classificationId
+    const data = await invModel.getInventoryByClassificationId(classification_id)
+    const grid = await utilities.buildClassificationGrid(data)
+    let nav = await utilities.getNav()
 
+    // Safety check: if data has results, use the name. If not, use a default.
+    const className = (data.length > 0) ? data[0].classification_name : "Unknown"
+
+    res.render("inventory/classification", {
+      title: className + " vehicles",
+      nav,
+      grid,
+    })
+  } catch (error) {
+    // This passes any database or code errors to your Error Handler in server.js
+    next(error)
+  }
+}
 
 module.exports = invCont
--- server.js
+++ server.js
@@ -11,6 +11,8 @@ dotenv.config() // Must be at the very top to load variables early
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const app = express()
+const utilities = require("./utilities/")
+const path = require("path")
 
 // Require internal modules after dotenv
 const baseController = require("./controllers/baseController")
@@ -20,6 +22,7 @@ const inventoryRoute = require("./controllers/inventoryRoute")
 /* ***********************
  * View Engine and Layouts
  *************************/
+app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
@@ -34,6 +37,25 @@ app.use("/inv", inventoryRoute)
 
 // Index route
 app.get("/", baseController.buildHome)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
